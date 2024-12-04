b45c412e225660747541b6252ec3d6e71716a70a
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-12-04 13:07:52 +0700

    chore: rombak structure

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"database/sql"
+
+	"github.com/gin-gonic/gin"
 	"github.com/halosatrio/xwing/config"
-	"github.com/halosatrio/xwing/handlers"
+	"github.com/halosatrio/xwing/routes"
 )
 
+// main function
 func main() {
 	config.LoadEnv()
 
 	db := config.ConnectDB()
 	defer db.Close()
 
-	r := handlers.SetupRouter(db)
+	r := setupRouter(db)
 	r.Run(":8080")
+}
+
+// setup app, define routes
+func setupRouter(db *sql.DB) *gin.Engine {
+	r := gin.Default()
+
+	// AS BASEPATH
+	v := r.Group("/v1")
+
+	// the routers
+	v.GET("/test", routes.Welcome)
+
+	return r
 }
---
+++ routes/general.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Welcome(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status":  http.StatusOK,
+		"message": "Welcome to Xwing!",
+	})
+}
