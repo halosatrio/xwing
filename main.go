09e02716acdebd65c8004cf0ec5501fc13595043
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-12-17 00:12:34 +0700

    fix: sepaarte private and public routes

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func setupRouter(db *sql.DB) *gin.Engine {
 	// AS BASEPATH
 	v1 := r.Group("/v1")
 	{
-    // health check
+		// health check
 		v1.GET("/test", func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{
 				"status":  http.StatusOK,
@@ -44,30 +44,36 @@ func setupRouter(db *sql.DB) *gin.Engine {
 			})
 		})
 
-		// Register Routes
+		// Register and Login [PUBLIC ROUTES]
 		v1.POST("/auth/register", routes.RegisterRoute(db))
 		v1.POST("/auth/login", routes.LoginUser(db))
-		v1.Use(utils.JWTAuth()).GET("/auth/user", routes.GetUser())
+
+		// [PRIVATE ROUTES]
+		v1.Use(utils.JWTAuth())
+
+		// auth user
+		v1.GET("/auth/user", routes.GetUser())
 
 		// Transaction Routes
-		v1.Use(utils.JWTAuth()).GET("/transaction", routes.GetAllTransactions(db))
-		v1.Use(utils.JWTAuth()).GET("/transaction/:id", routes.GetTransactionById(db))
-		v1.Use(utils.JWTAuth()).POST("/transaction/create", routes.PostCreateTransaction(db))
-		v1.Use(utils.JWTAuth()).PUT("/transaction/:id", routes.PutUpdateTransaction(db))
-		v1.Use(utils.JWTAuth()).DELETE("/transaction/:id", routes.DeleteTransaction(db))
-		v1.Use(utils.JWTAuth()).GET("/transaction/monthly-summary", routes.GetMonthlySummary(db))
+		v1.GET("/transaction", routes.GetAllTransactions(db))
+		v1.GET("/transaction/:id", routes.GetTransactionById(db))
+		v1.POST("/transaction/create", routes.PostCreateTransaction(db))
+		v1.PUT("/transaction/:id", routes.PutUpdateTransaction(db))
+		v1.DELETE("/transaction/:id", routes.DeleteTransaction(db))
+		v1.GET("/transaction/monthly-summary", routes.GetMonthlySummary(db))
 
 		// Report Routes
-		v1.Use(utils.JWTAuth()).GET("/report/quarter/essentials", routes.GetQuarterEssentials(db))
-		v1.Use(utils.JWTAuth()).GET("/report/quarter/non-essentials", routes.GetQuarterNonEssentials(db))
-		v1.Use(utils.JWTAuth()).GET("/report/quarter/shopping", routes.GetQuarterShopping(db))
-		v1.Use(utils.JWTAuth()).GET("/report/annual/cashflow", routes.GetAnnualCashflow(db))
+		v1.GET("/report/quarter/essentials", routes.GetQuarterEssentials(db))
+		v1.GET("/report/quarter/non-essentials", routes.GetQuarterNonEssentials(db))
+		v1.GET("/report/quarter/shopping", routes.GetQuarterShopping(db))
+		v1.GET("/report/annual/cashflow", routes.GetAnnualCashflow(db))
 		// GET Annual (WIP, this is for all months per caetgory)
-		// v1.Use(utils.JWTAuth()).GET("/report/annual", routes.GetAnnualReport(db))
+		//.GET("/report/annual", routes.GetAnnualReport(db))
 
 		// Asset Routes
-		v1.Use(utils.JWTAuth()).GET("/asset", routes.GetAsset(db))
-		v1.Use(utils.JWTAuth()).POST("/asset/create", routes.PostCreateAsset(db))
+		v1.GET("/asset", routes.GetAsset(db))
+		v1.POST("/asset/create", routes.PostCreateAsset(db))
+
 	}
 	return r
 }
